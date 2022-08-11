5e4e670b52cc0fd8b4293fdfc54eb82331c2f79b
Jee2110 <91604658+Jee2110@users.noreply.github.com>
2022-08-11 21:42:10 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,8 +1,8 @@
 let handler = async m => m.reply(`
-╭─「 Donasi • Pulsa 」
-│ •  [0895-3303-79186]
-│ •  [0896-5436-0447]
-╰────
+╭─「 Donasi Contact 」
+│ •  [0882-9186-5530][Panji|Dana]
+│ •  [0895-3951-07887][Seno|Pulsa]
+╰─
 `.trim()) // Tambah sendiri kalo mau
 handler.help = ['donasi']
 handler.tags = ['info']
