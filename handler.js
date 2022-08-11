149484aafec48686c8450ee73a768f1e3ecf83b3
Jee2110 <91604658+Jee2110@users.noreply.github.com>
2022-08-11 22:22:40 +0700

    Update handler.js

--- handler.js
+++ handler.js
@@ -290,7 +290,7 @@ module.exports = {
                     limitspam: 0,
                     money: 10000,
                     bank: 10000,
-                   ❤ health: 100,
+                    health: 100,
                     tiketcoin: 0,
                     healtmonster: 100,
                     armormonster: 0,
